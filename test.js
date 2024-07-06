10f055f942e90e64c8dd36bf300364ff077f5f1c
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-07-06 19:36:53 +0530

    Automate searching product

--- test.js
+++ test.js
@@ -1,20 +1,27 @@
 const {Builder, Key, By, until} = require('selenium-webdriver');
 
 async function openWebsite() {
-    let driver = await new Builder().forBrowser("chrome").build()
+    let driver = await new Builder().forBrowser("chrome").build();
 
     try {
       await driver.get('https://www.myntra.com/')
+      driver.manage().window().maximize();
+
       await driver.wait(until.titleIs('Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra'), 1000)
       .then(console.log("Opened website successfully"));
+
+      await driver.findElement(By.className("desktop-searchBar")).sendKeys("suitcase");
+      await driver.findElement(By.className("desktop-submit")).click();
+      await driver.findElement(By.xpath("//img[@title=\"Nasher Miles Vienna Hard-Sided Medium Trolley Suitcase\"]")).click();
 
     } catch(err) {
       console.log(err);
       
     }finally{
-        await driver.quit();
+        // await driver.quit();
     }
-
 }
+
+
 
 openWebsite();
