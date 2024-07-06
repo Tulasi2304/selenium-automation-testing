0c1bc3b65b34aeb6ec75bf86a50a1e246df374da
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-07-06 21:13:08 +0530

    Use mocha for testing

--- test.js
+++ test.js
@@ -1,27 +1,45 @@
 const {Builder, Key, By, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+var assert = require('assert');
 
-async function openWebsite() {
-    let driver = await new Builder().forBrowser("chrome").build();
+describe('Open Myntra and automate search', function () {
 
-    try {
-      await driver.get('https://www.myntra.com/')
-      driver.manage().window().maximize();
+  let driver;
+  let options = new chrome.Options();
 
-      await driver.wait(until.titleIs('Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra'), 1000)
-      .then(console.log("Opened website successfully"));
+  before(async () => {
 
-      await driver.findElement(By.className("desktop-searchBar")).sendKeys("suitcase");
-      await driver.findElement(By.className("desktop-submit")).click();
-      await driver.findElement(By.xpath("//img[@title=\"Nasher Miles Vienna Hard-Sided Medium Trolley Suitcase\"]")).click();
+    driver = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(options.detachDriver(true))
+      .build();
+    
+    await driver.get('https://www.myntra.com/')
+    
+  });
 
-    } catch(err) {
-      console.log(err);
-      
-    }finally{
-        // await driver.quit();
-    }
-}
+  it('Search for product', async () => {
 
+    driver.manage().window().maximize();
+    await driver.wait(until.titleIs('Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra'), 1000)
+    .then(console.log("Opened website successfully"));
 
+    await driver.findElement(By.className("desktop-searchBar")).sendKeys("suitcase");
+    await driver.findElement(By.className("desktop-submit")).click();
 
-openWebsite();
+  });
+
+  it('Open product', async () => {
+    await driver.findElement(By.xpath("//img[@title=\"Nasher Miles Vienna Hard-Sided Medium Trolley Suitcase\"]")).click();
+  });
+
+  // after(async () =>{ 
+  //   driver.quit();
+  // });
+
+});
+
+  // finally{
+  //     await driver.quit();
+  // }
+  // process.stdin.resume();
