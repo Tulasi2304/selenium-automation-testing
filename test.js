359ef125bd3b04a668b492e0656d5a40e96a39d8
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-07-07 12:23:51 +0530

    Handle new tab

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const {Builder, Key, By, until} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
-var assert = require('assert');
+// var assert = require('assert');
 
 describe('Open Myntra and automate search', function () {
 
@@ -18,7 +18,7 @@ describe('Open Myntra and automate search', function () {
     
   });
 
-  it('Search for product', async () => {
+  it('1. Search for product', async () => {
 
     driver.manage().window().maximize();
     await driver.wait(until.titleIs('Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra'), 1000)
@@ -29,17 +29,32 @@ describe('Open Myntra and automate search', function () {
 
   });
 
-  it('Open product', async () => {
+  it('2. Open product', async () => {
     await driver.findElement(By.xpath("//img[@title=\"Nasher Miles Vienna Hard-Sided Medium Trolley Suitcase\"]")).click();
   });
+
+  it('3. Add product to bag', async () => {
+
+    let currentTab = await driver.getWindowHandle();
+    const windows = await driver.getAllWindowHandles();
+    windows.forEach( async tab => {
+      if (!tab !== currentTab) {
+        await driver.switchTo().window(tab); 
+      }
+    }); 
+
+    // await driver.findElements(By.xpath("//a[@href=\"/24880030?skuId=79999012\"]"));
+    // await driver.findElement(By.className("pdp-add-to-bag")).click();
+    // await driver.findElement(By.className("desktop-iconBag")).click();
+
+  });
+
+  // it('4. Remove product from from', async () => {
+
+  // });
 
   // after(async () =>{ 
   //   driver.quit();
   // });
 
 });
-
-  // finally{
-  //     await driver.quit();
-  // }
-  // process.stdin.resume();
