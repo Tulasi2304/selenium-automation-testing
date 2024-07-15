3d2f9539137eb102c212cde93cc6b6f1c61a6cfc
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-07-15 21:25:04 +0530

    Add assertion to steps 3, 4 and 5

--- test.js
+++ test.js
@@ -23,15 +23,11 @@ describe('Open Myntra and automate search', function () {
   });
 
   it('1. Open website', async () => {
-
     driver.manage().window().maximize();
 
     let title = await driver.getTitle();
     assert.equal(title, 'Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra')
-    console.log("Opened website successfully\n");
-
-    // await driver.wait(until.titleIs('Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra'), 1000)
-    // .then(console.log("Opened website successfully"));
+    console.log("\nOpened website successfully\n");
 
   });
 
@@ -41,41 +37,47 @@ describe('Open Myntra and automate search', function () {
 
     let product = await driver.findElement(By.className("title-title")).getText();
     assert.equal(product, process.env.PRODUCT_NAME);
-    console.log("Product found successfully\n");
-
+    console.log("\nProduct found\n");
 
   });
 
   it('3. Open product', async () => {
-    // await driver.findElement(By.xpath("//img[@title = \"Nasher Miles Vienna Hard-Sided Medium Trolley Suitcase\"]")).click();
     await driver.findElement(By.xpath("//ul[@class=\"results-base\"]/li[1]")).click();
-  });
-
-  it('4. Add product to bag', async () => {
 
     let currentTab = await driver.getWindowHandle();
     const windows = await driver.getAllWindowHandles();
+
     windows.forEach( async newTab => {
       if (newTab !== currentTab) {
         await driver.switchTo().window(newTab); 
       }
-    }); 
+    });
 
+    let add = await driver.wait(until.elementLocated(By.className("pdp-add-to-bag")), 10000).getText();
+    assert.equal(add, "ADD TO BAG");
+    console.log("\nProduct opened\n");
+
+  });
+
+  it('4. Add product to bag', async () => {
     await driver.findElement(By.xpath("//div[@class=\"size-buttons-size-buttons\"]/div[1]")).click();
     await driver.findElement(By.className("pdp-add-to-bag")).click();
     await driver.findElement(By.className("desktop-iconBag")).click();
 
+    let offers = await driver.findElement(By.xpath("//div[@class=\"offersV2-base-title \"]")).getText();
+    assert.equal(offers, "Available Offers");
+    console.log("\nGo to bag\n");
+
   });
 
-  // it('Read items in bag', async () => {
-  //   items = await driver.findElement(By.xpath("//div[@class=\"cartItemsList\"]"))
-  // });
-
   it('5. Remove product from bag', async () => {
-
     await driver.findElement(By.className("itemContainer-base-closeIcon")).click();
     await driver.findElement(By.className("inlinebuttonV2-base-action confirmOrCancelModal-buttonClass")).click();
 
+    let bagEmpty = await driver.wait(until.elementLocated(By.xpath('//div[@class="emptyCart-base-emptyText"]')), 10000).getText();
+    assert.equal(bagEmpty, "Hey, it feels so light!");
+    console.log("\nBag emptied\n");
+    
     // await driver.navigate().back();
 
   });
