e8f8a2d2f9b2ae4e76c755c13056845801d37ce9
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-07-15 20:49:51 +0530

    Add assertion for steps 1 and 2

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const {Builder, Key, By, until} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const assert = require('assert');
+
 require("dotenv").config();
-// var assert = require('assert');
 
 describe('Open Myntra and automate search', function () {
 
@@ -16,28 +17,41 @@ describe('Open Myntra and automate search', function () {
       .forBrowser("chrome")
       .setChromeOptions(options.detachDriver(true))
       .build();
-    console.log(process.env.LINK);
+
     await driver.get(process.env.LINK);
     
   });
 
-  it('1. Search for product', async () => {
+  it('1. Open website', async () => {
 
     driver.manage().window().maximize();
-    await driver.wait(until.titleIs('Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra'), 1000)
-    .then(console.log("Opened website successfully"));
 
-    await driver.findElement(By.className("desktop-searchBar")).sendKeys(process.env.PRODUCT_NAME);
-    await driver.findElement(By.className("desktop-submit")).click();
+    let title = await driver.getTitle();
+    assert.equal(title, 'Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra')
+    console.log("Opened website successfully\n");
+
+    // await driver.wait(until.titleIs('Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra'), 1000)
+    // .then(console.log("Opened website successfully"));
 
   });
 
-  it('2. Open product', async () => {
+  it('2. Search for product', async () => {
+    await driver.findElement(By.className("desktop-searchBar")).sendKeys(process.env.PRODUCT_NAME);
+    await driver.findElement(By.className("desktop-submit")).click();
+
+    let product = await driver.findElement(By.className("title-title")).getText();
+    assert.equal(product, process.env.PRODUCT_NAME);
+    console.log("Product found successfully\n");
+
+
+  });
+
+  it('3. Open product', async () => {
     // await driver.findElement(By.xpath("//img[@title = \"Nasher Miles Vienna Hard-Sided Medium Trolley Suitcase\"]")).click();
     await driver.findElement(By.xpath("//ul[@class=\"results-base\"]/li[1]")).click();
   });
 
-  it('3. Add product to bag', async () => {
+  it('4. Add product to bag', async () => {
 
     let currentTab = await driver.getWindowHandle();
     const windows = await driver.getAllWindowHandles();
@@ -57,7 +71,7 @@ describe('Open Myntra and automate search', function () {
   //   items = await driver.findElement(By.xpath("//div[@class=\"cartItemsList\"]"))
   // });
 
-  it('4. Remove product from bag', async () => {
+  it('5. Remove product from bag', async () => {
 
     await driver.findElement(By.className("itemContainer-base-closeIcon")).click();
     await driver.findElement(By.className("inlinebuttonV2-base-action confirmOrCancelModal-buttonClass")).click();
